351855afab08703068011f25ba7aade153fe464b
northtape <94729796+nrthtape@users.noreply.github.com>
2022-07-04 14:28:43 +0300

    update

--- script/game.js
+++ script/game.js
@@ -695,8 +695,8 @@ function resizeGame(){
 
     // Get the dimensions of the viewport
     viewport = {
-        width: window.innerWidth,
-        height: window.innerHeight
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
     };
 
     // Determine game size
